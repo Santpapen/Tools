bbaa120ea4602375f46012cc70592f43db9fde03
Santpapen <lotopara@gmail.com>
2017-09-05 18:44:00 +0200

    Update personal_tail_f.cc

--- personal_tail_f.cc
+++ personal_tail_f.cc
@@ -1,6 +1,8 @@
 #include <fstream>
+#include <stdio.h>
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 ifstream infile;
@@ -11,35 +13,45 @@ int openFile(int n);
 string line;
 int ini =0 ;
 
-
 /* Init program */
 
 int main()
 
 {
-char data[100];
-string c = "run";
-while (c == "run"){
-        ini=openFile(ini);
+string c = "run"; // Iniciador del programa
+while (c == "run"){ 
+	ini=openFile(ini);
 }
-/*infile.close();*/
 return 0;
 }
 
 
 int openFile(int n) {
-infile.open("dest.txt");
-int numLine = 0;
+infile.open("pruebasDHCP"); /* Fichero a filtrar */
+int numLine = 0; /* Iniciador de contador */
+char delimiter[100]; //Variable 
 if (infile.is_open()){
-        while (getline (infile,line) )
-        {
-                numLine =numLine +1;
-                if(n < numLine){
-                cout << line << endl;
-                }
-        }
-
-        infile.close();
+	while (getline (infile,line) )
+	{
+		numLine =numLine +1; 
+		if(n < numLine){
+			istringstream iss(line);
+			do 
+			{
+				string subs; //Split del line
+				iss >> subs;  
+				for (int i=0;i<subs.length();i++){
+					delimiter[i]=subs[i]; // Pasa la string a char
+				}
+				if (strcmp(delimiter,"(DHCPREQUEST)")==0){ /* Filtrado por palabra */
+				cout << "Substrings: " << line << endl; // Imprime la linea 
+				}
+				memset(delimiter,0,100); /* RESETEO */	
+			}while (iss);
+		}
+	}
+	
+	infile.close();
 }
 return numLine;
 }
