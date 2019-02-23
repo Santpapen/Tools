8b4d96c28aca24b74b9cbfebeb3a622aba026af6
Santpapen <lotopara@gmail.com>
2019-02-24 00:18:12 +0100

    Add files via upload

---
+++ tmuxSession.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Script inicio tmux
+
+if(tmux list-session)>/dev/null 2>&1
+then
+	local -i opens=$(tmux list-session |cut -c1|wc -l)
+	echo $opens
+	if  tmux list-session |cut -c1|wc -l|grep 1
+	then 
+		session=$(tmux list-session |cut -c1)
+                tmux attach-session -t $session
+	else 
+	        tmux list-session
+                echo "Seleccionar una opcion:"
+                read x
+                tmux attach-session -t $x
+	fi 
+else
+	tmux
+fi
+	
